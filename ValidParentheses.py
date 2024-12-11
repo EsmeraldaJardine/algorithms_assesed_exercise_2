9dff531a4d3374d6028efd79f7f94c59cba75730
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-12-11 16:47:47 +0000

    first draft

---
+++ ValidParentheses.py
@@ -0,0 +1,35 @@
+def process_string(s):
+    bracket_pairs = {'{': '}', '[': ']', '(': ')'}
+    stack = []
+    if s == "":
+        print("The expression is not valid")
+        return
+    for bracket in s:
+        #pushing opening brackets to stack
+        if bracket in bracket_pairs.keys():
+            stack.append(bracket)
+        else:
+            #if closing bracket is found, check the stck is not empty
+            if stack == []:
+                print("The expression is not valid")
+                return
+            #check the last element in stack is the opening bracket for the closing bracket
+            elif bracket_pairs[stack[-1]] == bracket:
+                    stack.pop()
+            else:
+                print("The expression is not valid")
+                return
+    #check if stack is empty after intreating through the string
+    if stack != []:
+        print("The expression is not valid")
+    else:
+        print("The expression is valid")
+
+
+
+def main():
+    s = "{}" #add string here
+    process_string(s) 
+if __name__ == "__main__":
+    main()
+        
