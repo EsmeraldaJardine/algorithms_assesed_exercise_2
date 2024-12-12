98501ee8a0b662e105062caf894b38d00830fa70
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-12-12 13:29:49 +0000

    cleaned up

--- BSTHeightHistogram.py
+++ BSTHeightHistogram.py
@@ -2,14 +2,12 @@ from itertools import permutations
 
 class BSTHeightHistogram:
 
-
     def make_bst(self, permutation):
         bst = None
         for key in permutation:
-            #print("key: ", key)
             bst = self.insert_node(bst, key)
-            #print("bst: ", bst)
         return bst
+
 
     def insert_node(self, bst, key):
         if bst is None:
@@ -20,6 +18,7 @@ class BSTHeightHistogram:
             bst[2]= self.insert_node(bst[2], key)
         return bst
     
+
     def calculate_height(self,bst):
         if bst is None or (bst[1] is None and bst[2] is None):
             return 0
@@ -27,38 +26,25 @@ class BSTHeightHistogram:
         l_subtree_height = self.calculate_height(bst[1])
         r_subtree_height = self.calculate_height(bst[2])
         return 1+ max(l_subtree_height,r_subtree_height)
-
-            
-         
-                
+    
 
     def get_permutations(self, n):
             n_permutations = list(permutations(range(1,n+1)))
-            #print("Permutations: ", n_permutations)
             return n_permutations
-                #n_permutations = list(permutations(range(1,i+1)))
-                #print(n_permutations)
-                # for j in range(len(n_permutations)):
-                #     #print(n_permutations[j])
-                #     BSTHeightHistogram.make_bst(list(n_permutations[j]))
-
-
-
 
 
 def main():
     instance = BSTHeightHistogram()
     n = int(input("Enter a positive integer: "))
-    permutations = instance.get_permutations(n)
     height_count = {}
     height_total = 0
     bst_count = 0
+
+    permutations = instance.get_permutations(n)
+
     for permutation in permutations:
-        print("permutations: ",permutation)
         bst = instance.make_bst(permutation)
-        print(f"Permutation: {permutation} -> BST: {bst}")
         height = instance.calculate_height(bst)
-        print("height: ", height)
         if height not in height_count:
             height_count[height]= 1
         else:
@@ -66,19 +52,17 @@ def main():
 
         height_total += height
         bst_count+=1
+
     average_height = height_total/bst_count
-    print("height   |   frequency")
-    print("_______________________")
+
+    print("\nheight   |   frequency")
+    print("----------------------")
     for height in sorted(height_count.keys()):
-        print(height,"       |  ", height_count[height]) #cannot get counts for heights 0 and 1
+        print(height,"       |  ", height_count[height]) #cannot get counts for heights 0 and 1??
 
     print(" Average height of BTSs: ", average_height)
 
-        
-    
-    
-        
-        
+              
 if __name__ == "__main__":
     main()
 
